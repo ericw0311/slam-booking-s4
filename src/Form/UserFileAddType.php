906bd8af621fe8e5ff8860ca5a0b2020b684f1b1
ericw0311 <eric.willard@slam-data.net>
2018-08-04 23:34:57 +0200

    20180804_pm1

--- src/Form/UserFileAddType.php
+++ src/Form/UserFileAddType.php
@@ -6,6 +6,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -16,8 +17,15 @@ class UserFileAddType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
 		$builder->add('email', HiddenType::class)
-			->add('lastName', TextType::class, array('label' => 'user.lastName', 'translation_domain' => 'messages'))
+			->add('accountType', ChoiceType::class, array(
+				'label' => 'user.account.type',
+				'translation_domain' => 'messages',
+				'choices' => array('INDIVIDUAL' => 'INDIVIDUAL', 'ORGANISATION' => 'ORGANISATION'),
+				'choice_label' => function ($value, $key, $index) { return 'user.account.type.'.$key; }
+			))
 			->add('firstName', TextType::class, array('label' => 'user.firstName', 'translation_domain' => 'messages'))
+			->add('lastName', TextType::class, array('label' => 'user.lastName', 'translation_domain' => 'messages'))
+			->add('uniqueName', TextType::class, array('label' => 'user.organisation.name', 'translation_domain' => 'messages', 'required' => false))
 			->add('administrator', HiddenType::class, array('data' => 0));
 	}
 
